7409e6e948cac4ca79f2995e770ac2d4a3936668
unknown <Jatin Chaurawar>
2022-06-28 12:00:57 +0530

    initiaklize

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Sangeet");
+let songs = [
+    { songName: "Salam-e-Ishq", filePath: "song/1.mp3"
+        coverPath: "cover/1.jpg" },
+]
+let audioElement = Audio('1.mp3');
+//audioElement.play();
